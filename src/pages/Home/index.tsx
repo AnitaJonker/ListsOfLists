0cdaa8e29f9427670611b89d3008a08256f02ae5
AnitaJonker <anitaroneljonker@gmail.com>
2025-05-17 15:39:27 +0200

    first page for list of lists

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+const Home = () => {
+  return (
+    <>
+    <div >
+     
+      <ul className="flex gap-10 w-full  justify-start items-start border-3 text-black bg-white">
+        <li>Index 1</li>
+        <li>Index 2</li>
+        <li>Index 3</li>
+      </ul>
+
+    </div>
+    </>
+  )
+}
+
+export default Home;
